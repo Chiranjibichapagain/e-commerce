5ae3768995012223b49d12be488104311d27d744
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2020-10-01 23:09:56 +0300

    product tile ready

--- src/components/ProductTile/index.tsx
+++ src/components/ProductTile/index.tsx
@@ -2,37 +2,41 @@ import React from 'react'
 
 import useStyle from './styles'
 import { CardProps } from '../../types/componentTypes'
-import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button';
+import Paper from "@material-ui/core/Paper";
 import Typography from '@material-ui/core/Typography';
-import FavoriteIcon from '@material-ui/icons/Favorite'
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 
 const ProductTile = ({ product }: CardProps) => {
     const classes = useStyle()
     return (
-        <Paper elevation={3} className={classes.root}>
-            <div className={classes.imageBox} >
-                <img className={classes.image} src={product.image} alt={`image of ${product.title}`} />
-            </div>
-            <div className={classes.info}  >
-                <div className={classes.name} >
-                    {product.title}
-                </div>
-                <div className={classes.price} >
-                    {`${product.price}$`}
-                </div>
-            </div>
-            <div className={classes.description} >
-                <Typography>{product.description}</Typography>
-            </div>
-            <div className={classes.actions}  >
-                <FavoriteIcon className={classes.fav} />
-                <button className={classes.button}>Add to Cart</button>
-            </div>
-
-        </Paper>
-    )
+      <Paper elevation={5} className={classes.root}>
+        <div className={classes.imageBox}>
+          <img
+            className={classes.image}
+            src={product.image}
+            alt={`image of ${product.title}`}
+          />
+        </div>
+        <div className={classes.info}>
+          <div className={classes.name}>{product.title}</div>
+          <div className={classes.price}>{`${product.price}$`}</div>
+        </div>
+        <div className={classes.description}>
+          <Typography>{product.description}</Typography>
+        </div>
+        <div className={classes.actions}>
+          <button className={classes.toCartButton}>
+            <AddShoppingCartIcon />
+            <Typography>Add-to-cart</Typography>
+          </button>
+          <button className={classes.viewButton}>
+            <VisibilityIcon />
+            <Typography>View-item</Typography>
+          </button>
+        </div>
+      </Paper>
+    );
 }
 
 export default ProductTile
--- src/components/ProductTile/styles.tsx
+++ src/components/ProductTile/styles.tsx
@@ -1,81 +1,101 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "350px",
+    borderRadius: "8px",
+    margin: " 40px 20px",
+    transition: "0.5s ease-in-out",
+
+    "&:hover": {
+      boxShadow: "0px 0px 1px black",
+    },
+  },
+
+  imageBox: {
+    display: "flex",
+    justifyContent: "center",
+    marginButton: "5px",
+  },
+  image: {
+    maxWidth: "350px",
+    maxHeight: "150px",
+  },
+
+  info: {
+    margin: "30px 10px 10px 10px",
+    borderTop: "1.5px solid grey",
+    padding: "15px 10px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  name: {
+    fontSize: "18px",
+  },
+
+  price: {
+    fontWeight: "bold",
+  },
+
+  description: {
+    padding: "10px 15px",
+  },
+
+  actions: {
+    borderRadius: "0px 0px 6px 6px",
+    backgroundColor: "#E9E9E9",
+    position: "relative",
+    top: "28px",
+    padding: "20px 15px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  toCartButton: {
+    width: "150px",
+    backgroundColor: "#299B00",
+    border: "none",
+    padding: "12px",
+    borderRadius: "4px",
+    fontWeight: "bold",
+    color: "white",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    transition: "0.3s ease-in-out",
+
+    "&:hover": {
+      backgroundColor: "white",
+      color: "orangered",
+      border: "1px solid orangered",
+    },
+  },
+  viewButton: {
+    width: "150px",
+    backgroundColor: "#0040C3",
+    border: "none",
+    padding: "12px",
+    borderRadius: "4px",
+    fontWeight: "bold",
+    color: "white",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    transition: "0.3s ease-in-out",
+
+    "&:hover": {
+      backgroundColor: "white",
+      color: "orangered",
+      border: "1px solid orangered",
+    },
+  },
+
+  "@media only screen and (max-width: 768px)": {
     root: {
-        width: "350px",
-        boxShadow: "0px 0px 6px #848383",
-        borderRadius: "8px",
-        margin: "20px",
+      width: "80vw",
     },
-
-
-    imageBox: {
-        display: "flex",
-        justifyContent: "center",
-        marginButton: "5px"
-
-    },
-    image: {
-        maxWidth: "350px",
-        maxHeight: "150px"
-    },
-
-    info: {
-        margin: '30px 10px 10px 10px',
-        borderTop: "1.5px solid grey",
-        padding: '15px 10px',
-        display: 'flex',
-        justifyContent: 'space-between'
-    },
-    name: {
-        fontSize: '18px'
-    },
-
-    price: {
-        fontWeight: 'bold'
-    },
-
-    description: {
-        padding: '10px 15px'
-    },
-
-
-    actions: {
-        borderRadius: '0px 0px 6px 6px',
-        backgroundColor: 'grey',
-        position: 'relative',
-        top: '28px',
-        padding: '10px',
-        display: "flex",
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-
-    fav: {
-        fontSize: "40px",
-        color: 'red',
-        '&:hover': {
-            color: 'green',
-        }
-    },
-
-    button: {
-        backgroundColor: "orangered",
-        border: 'none',
-        padding: '10px',
-        borderRadius: '4px',
-        fontWeight: 'bold',
-
-    },
-
-    '@media only screen and (max-width: 768px)': {
-        root: {
-            width: '80vw',
-        },
-
-    },
-
-
-}))
+  },
+}));
 
 export default useStyles
